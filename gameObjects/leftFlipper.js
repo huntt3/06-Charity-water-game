a44300c0d1b90bc76e4e4417d40b12864de3783a
Trevor Hunt <157053920+huntt3@users.noreply.github.com>
2025-06-23 00:47:22 +0000

    Commit 13. Added oop modularity.

---
+++ gameObjects/leftFlipper.js
@@ -0,0 +1,21 @@
+// LeftFlipper class for modular OOP
+class LeftFlipper {
+  constructor(x, y, length, width, world, Matter) {
+    this.body = Matter.Bodies.rectangle(x, y, length, width, {
+      isStatic: false,
+      chamfer: { radius: 8 },
+      render: { fillStyle: '#F16061' }
+    });
+    this.hinge = Matter.Constraint.create({
+      bodyA: this.body,
+      pointB: { x: x - length/2 + 5, y: y },
+      pointA: { x: -length/2 + 5, y: 0 },
+      stiffness: 1,
+      length: 0
+    });
+    Matter.Composite.add(world, [this.body, this.hinge]);
+    Matter.Body.setAngularVelocity(this.body, 0);
+    Matter.Body.setDensity(this.body, 0.1);
+    this.body.restitution = 0.25;
+  }
+}
---
+++ gameObjects/wall.js
@@ -0,0 +1,7 @@
+// Wall class for modular OOP
+class Wall {
+  constructor(x, y, width, height, world, Matter) {
+    this.body = Matter.Bodies.rectangle(x, y, width, height, { isStatic: true });
+    Matter.Composite.add(world, this.body);
+  }
+}
